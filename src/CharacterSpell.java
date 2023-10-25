f83b82a06d0479594022a922c66c42801cded43b
KIL Bismuth <kiralowrey@gmail.com>
2023-10-25 10:40:13 -0700

    outline spells and characterspells

--- src/CharacterSpell.java
+++ src/CharacterSpell.java
@@ -6,4 +6,52 @@
 public class CharacterSpell extends Spell
 {
 
+	//////// FIELDS ////////
+	// if character has spell prepared
+	private boolean isPrepared;
+	// if it is always prepared
+	private boolean alwaysPrepared;
+	// if the user has favorited it for that character
+	private boolean isFavorite;
+	// character notes such as "can only cast once a day but it doesn't cost a spell slot"
+	private String characterNotes;
+	
+	//////// SETTERS AND GETTERS ////////
+	
+	/**
+	 * get isPrepared
+	 */
+	
+	/**
+	 * set isPrepared
+	 */
+	
+	/**
+	 * get alwaysPrepared
+	 */
+	
+	/**
+	 * set alwaysPrepared
+	 */
+	
+	/**
+	 * get isFavorite
+	 */
+	
+	/**
+	 * set isFavorite
+	 */
+	
+	/**
+	 * get characterNotes
+	 */
+	
+	/**
+	 * set characterNotes
+	 */
+	
+	
+	
+	//////// CONSTUCTORS ////////
+	
 }
--- src/Spell.java
+++ src/Spell.java
@@ -5,5 +5,57 @@
  */
 public class Spell
 {
-
+	//////// FIELDS ////////
+	// name of spell
+	private String spellName;
+	// spell level 0-9
+	private int spellLevel;
+	// what school of magic it is
+	private String school; // SWITCH to ENUM later that is the 8 SCHOOLS
+	// how long it takes to cast
+	private String castTime;
+	// spell's range
+	private String range;
+	// spell has a verbal component
+	private boolean isV;
+	// spell has a "somatic" (hand gesture) component
+	private boolean isS;
+	// spell has a material component
+	private boolean isM;
+	// if spell has a material component, what are the materials?
+	private String materials;
+	// how long does it last?
+	private String duration;
+	// can it be case a a ritual?
+	private boolean isRitual;
+	// does it require concentration?
+	private boolean isConcentration;
+	// is it an attack roll?
+	private boolean isAttack; 
+	// what kind of saving throw (null if isAttack = true)
+	private String saveType; // SWITCH to ENUM later that is the six stats with NULL for not a save
+	// the description of the spell in the book
+	private String spellDescription;
+	// the descriiption of what te spell does at higher levels
+	private String higherLevels;
+	// if it's SRD or otherwise
+	private String source; // SWITCH TO ENUM THAT IS SRD, WOTC, OR HB
+	// for tracking them:
+	private final int index;
+	private static int counter;
+	
+	//////// CONSTRUCTORS ////////
+	
+	// add a spell from the database
+	public Spell (String source, int index)
+	{
+		
+	}
+	
+	// user adds a spell wholecloth
+	public Spell (String source, String spellName)
+	{
+		
+	}
+	
 }
