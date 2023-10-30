0577fa3ac7309b6fbf791edf8e1eb653e861a957
KIL Bismuth <kiralowrey@gmail.com>
2023-10-29 19:31:15 -0700

    Update Character.java

--- src/Character.java
+++ src/Character.java
@@ -5,5 +5,24 @@
  */
 public class Character
 {
-
+	//////// FIELDS ////////
+	// name of character
+	private String characterName;
+	// character's level
+	private int characterLevel;
+	// 
+	private int attackBonus;
+	//
+	private int spellSaveDC;
+	//
+	private boolean spellPoints;
+	//
+	private boolean sorceryPoints;
+	//
+	// if spellPoints == true they will have a spellPoints object
+	// if sorceryPoints == true they will have a sorceryPoints obj
+	
+	// they will also have a lot of spell slot objects
+	
+	// they will also have a collection of character spell objects
 }
